dd8c51f90ffe3d247e258888452be68c356d1e29
robcronin <croninro@tcd.ie>
2022-12-26 16:12:40 +0000

    Update newDay script

--- scripts/newDay.sh
+++ scripts/newDay.sh
@@ -6,10 +6,12 @@ if [ $? -ne 0 ] ; then
   exit
 fi
 
-echo "export const day$1 = (input: number[]) => {
+echo "
+export const day$1 = (input: string[]) => {
   return $1;
 };
-export const day$1part2 = (input: number[]) => {
+
+export const day$1part2 = (input: string[]) => {
   return $1;
 };
 " >> src/day$1/day$1.ts
@@ -17,20 +19,17 @@ export const day$1part2 = (input: number[]) => {
 echo "import { logAnswer } from '../utils/logging';
 import { parseInput } from '../utils/input';
 import { day$1, day$1part2 } from './day$1';
-import { data } from './day$1.data';
+import { data, testData } from './day$1.data';
 
-const testString = '';
-const testData = parseInput(testString);
-
-describe('day $1', () => {
-  it('test cases', () => {
+describe.only('day $1', () => {
+  it.only('test cases', () => {
+    console.log(testData)
     expect(day$1(testData)).toBe($1);
   });
 
   it('answer', () => {
     const answer = day$1(data);
     logAnswer(answer, $1, 1);
-    expect(typeof answer).toBe('number');
     expect(answer).toBe($1);
   });
 });
@@ -43,16 +42,18 @@ describe('day $1 part 2', () => {
   it('answer', () => {
     const answer = day$1part2(data);
     logAnswer(answer, $1, 2);
-    expect(typeof answer).toBe('number');
     expect(answer).toBe($1);
   });
 });" >> src/day$1/day$1.test.ts
 
 echo "import { parseInput } from '../utils/input';
 
+const testString = '';
 const input = '';
 
-export const data = parseInput(input) as number[];" >> src/day$1/day$1.data.ts
+export const testData = parseInput(testString) as string[];
+export const data = parseInput(input) as string[];" >> src/day$1/day$1.data.ts
 
+code src/day$1/day$1.data.ts src/day$1/day$1.ts src/day$1/day$1.test.ts
 yarn test day$1
 exit
